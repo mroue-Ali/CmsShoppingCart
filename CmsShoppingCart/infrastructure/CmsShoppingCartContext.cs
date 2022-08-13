101fbbc46fffd489626310f5b5570368dcd7d6a4
MrouAli <mrou2001ali@gmail.com>
2022-08-13 16:33:07 +0300

    db

---
+++ CmsShoppingCart/infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.infrastructure
+{
+    public class CmsShoppingCartContext :DbContext 
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+            :base(options)
+        {
+
+        }
+
+
+    }
+}
